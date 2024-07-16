e535bc3a9893d1e04fbe7d9efb8cc8c75696175c
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-07-16 19:54:09 -0300

    feat: add cors middleware

--- router/router.go
+++ router/router.go
@@ -4,12 +4,14 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jsGolden/frete-rapido-api/handlers/quotes"
+	"github.com/jsGolden/frete-rapido-api/middlewares"
 )
 
 func SetupRouter() *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
+	router.Use(middlewares.Cors())
 
 	router.Post("/quote", quotes.CreateQuote)
 	router.Get("/metrics", quotes.GetQuoteMetrics)
